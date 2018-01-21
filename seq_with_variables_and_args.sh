0189ccec45690aeae15df39df579194d093e1b8a
sborrego <sborrego@users.noreply.github.com>
2018-01-21 13:52:41 -0800

    Update seq_with_variables_and_args.sh

--- seq_with_variables_and_args.sh
+++ seq_with_variables_and_args.sh
@@ -1,7 +1,11 @@
 
 #!/bin/bash
 
-for NUMBER in `seq $1 $2 $3`; do
+NSTART_SEQ=$1
+NSTEP_SEQ=$2
+NEND_SEQ=$3
+
+for NUMBER in `seq ${NSTART_SEQ} ${NSTEP_SEQ} ${NEND_SEQ}`; do
         echo $NUMBER
         echo $NUMBER >> seq.txt
 done
