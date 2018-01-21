b4ae1231c34e26bab60780105b89caa83cd08ba2
sborrego <sborrego@users.noreply.github.com>
2018-01-20 23:56:54 -0800

    Create 06_sra_fastq.sh

---
+++ 06_sra_fastq.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+#$ -N sra_data_dump
+#$ -o sra.out
+#$ -e sra.err
+#$ -q free64
+#$ -m beas
+
+module load SRAToolKit
+
+# Retrieving SRA data and converting to fastq
+fastq-dump -X 5 SRR390728
