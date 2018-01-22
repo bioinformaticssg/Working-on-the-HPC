da7ad65a82de9fb2f78a2d34aa1a31530d8efcaf
sborrego <sborrego@users.noreply.github.com>
2018-01-21 20:22:03 -0800

    Update 10_sra_fastq.sh

--- 10_sra_fastq.sh
+++ 10_sra_fastq.sh
@@ -9,7 +9,7 @@
 
 module load SRAToolKit
 
-# Retrieving SRA data and converting to fastq
-# -X prints only the first 5 spots
+# Retrieving SRA data and converting to fastq format
+# -X prints only the first 5 spots (20 lines)
 # Data will show up in directory as SRR390728.fastq
 fastq-dump -X 5 SRR390728
