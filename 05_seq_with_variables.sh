cac8923a3082cfac724bf1df7ce9d6b2fa03bd3e
sborrego <sborrego@users.noreply.github.com>
2018-01-21 18:02:55 -0800

    Update 05_seq_with_variables.sh

--- 05_seq_with_variables.sh
+++ 05_seq_with_variables.sh
@@ -1,11 +1,12 @@
 
 #!/bin/bash
 
+# Assigning the following variables with numbers to start, step, and end the sequence with. When you see these names the value assigned will replace them.
 NSTART_SEQ=0
 NSTEP_SEQ=5
 NEND_SEQ=30
 
 for NUMBER in `seq ${NSTART_SEQ} ${NSTEP_SEQ} ${NEND_SEQ}`; do
-        echo $NUMBER
-        echo $NUMBER >> seq.txt
+        echo $NUMBER # prints number sequence to stdout (terminal)
+        echo $NUMBER >> seq.txt # prints number sequence to file
 done
