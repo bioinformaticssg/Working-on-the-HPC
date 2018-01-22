802646ab7a167273e02319ecd988829f187e44c7
sborrego <sborrego@users.noreply.github.com>
2018-01-21 20:41:27 -0800

    Update 09_sra_stdout.sh

--- 09_sra_stdout.sh
+++ 09_sra_stdout.sh
@@ -5,7 +5,9 @@
 #$ -e sra.err       # file name to print standard error messages to. These messages would normally be printed to the the terminal
 #$ -q free64        # request cores from the free64 queue.
 #$ -m beas          # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
+#$ -ckpt blcr       # (c)checkpoint: writes a snapshot of a process to disk, (r)estarts the process after the checkpoint is complete
 
+module load blcr
 module load SRAToolKit
 
 # Retrieving SRA data and converting it to fastq format
