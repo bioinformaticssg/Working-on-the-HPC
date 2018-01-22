4234e67cda212fc37bd01c561d866b82f9b99d70
sborrego <sborrego@users.noreply.github.com>
2018-01-21 20:21:17 -0800

    Update 09_sra_stdout.sh

--- 09_sra_stdout.sh
+++ 09_sra_stdout.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#$ -N sra_data_dump
-#$ -o sra.out
-#$ -e sra.err
-#$ -q free64
-#$ -m beas
+#$ -N sra_data_dump # name of the job
+#$ -o sra.out       # contains what would normally be printed to stdout (the terminal)
+#$ -e sra.err       # file name to print standard error messages to. These messages would normally be printed to the the terminal
+#$ -q free64        # request cores from the free64 queue.
+#$ -m beas          # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
 
 module load SRAToolKit
 
 # Retrieving SRA data and converting it to fastq format
 # -X prints the first five spots 
-# -Z prints to standard out
+# -Z prints to standard out (which we have named sra.out above)
 fastq-dump -X 5 -Z SRR390728
