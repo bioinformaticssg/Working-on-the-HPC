a91fb4b636f85d4113b51a014cf47fc4facd1266
sborrego <sborrego@users.noreply.github.com>
2018-01-21 17:57:41 -0800

    Update 04_basic_dir_with_variables.sh

--- 04_basic_dir_with_variables.sh
+++ 04_basic_dir_with_variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIRECTORY="new_hello_directory" # assigns the variable called DIRECTORY to new_hello_directory
+DIRECTORY="new_hello_directory" # assigns the variable called DIRECTORY to the string "new_hello_directory"
 
-mkdir ${DIRECTORY} # makes a new directory. When you see ${DIRECTORY} it will use new_hello_directory
+mkdir ${DIRECTORY} # makes a new directory. When you see ${DIRECTORY} it will use the string "new_hello_directory"
 echo "Hello, world!" > ${DIRECTORY}/hello_file.txt # prints message to a file in the indicated path
