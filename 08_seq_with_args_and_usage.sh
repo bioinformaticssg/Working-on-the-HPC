b77646908d06415515908efa63be67907c7d945f
sborrego <sborrego@users.noreply.github.com>
2018-01-21 19:22:24 -0800

    Update 08_seq_with_args_and_usage.sh

--- 08_seq_with_args_and_usage.sh
+++ 08_seq_with_args_and_usage.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # If the arguments given do not equal (-ne) 3, then the following message 
-# will appear to provide the user information about the aruguments that should be provided
+# will appear to provide the user information about the arguments that should be provided
 if [ $# -ne 3 ]; then
     echo "usage: nstart_seq nstep_seq nend_seq"
     exit 1
 fi
 
+# Instead of assigning variables, here the command line parameters are used directly (indicated by $1, $2, and $3)
 for NUMBER in `seq $1 $2 $3`; do
         echo $NUMBER # prints to stdout
         echo $NUMBER >> seq.txt # prints to file named seq.txt
