79c6e2ae5cb9a90ceaf058f81b2923adac698096
sborrego <sborrego@users.noreply.github.com>
2018-01-21 23:26:14 -0800

    Update 09_sra_stdout.sh

--- 09_sra_stdout.sh
+++ 09_sra_stdout.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#$ -N sra_data_dump_09 # name of the job
-#$ -o sra.out       # contains what would normally be printed to stdout (the terminal)
-#$ -e sra.err       # file name to print standard error messages to. These messages would normally be printed to the the terminal
-#$ -q free64        # request cores from the free64 queue.
-#$ -m beas          # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
-#$ -ckpt blcr       # (c)checkpoint: writes a snapshot of a process to disk, (r)estarts the process after the checkpoint is complete
+#$ -N sra_data_dump_09  # name of the job
+#$ -o sra.out           # contains what would normally be printed to stdout (the terminal)
+#$ -e sra.err           # file name to print standard error messages to. These messages would normally be printed to the the terminal
+#$ -q free64            # request cores from the free64 queue.
+#$ -m beas              # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
+#$ -ckpt blcr           # (c)checkpoint: writes a snapshot of a process to disk, (r)estarts the process after the checkpoint is complete
 
 module load blcr
 module load SRAToolKit
