883e4ae278ad3f31ecc2bb83f06d2fca6d96b809
sborrego <sborrego@users.noreply.github.com>
2018-01-21 18:08:42 -0800

    Update 06_seq_with_variables_and_args.sh

--- 06_seq_with_variables_and_args.sh
+++ 06_seq_with_variables_and_args.sh
@@ -1,11 +1,13 @@
 
 #!/bin/bash
 
-NSTART_SEQ=$1
+# Command line parameters given when the script is invoked are indicated by $# (ex: $1, $2) based on the order they are given.
+# Here we are assigning the command line parameter to a variable so we don't have the change script at the bottom.
+NSTART_SEQ=$1 
 NSTEP_SEQ=$2
 NEND_SEQ=$3
 
 for NUMBER in `seq ${NSTART_SEQ} ${NSTEP_SEQ} ${NEND_SEQ}`; do
-        echo $NUMBER
-        echo $NUMBER >> seq.txt
+        echo $NUMBER # prints to stdout
+        echo $NUMBER >> seq.txt # prints to file
 done
