6fba3cf7a9decbdefae562bec4a1fd2b672921b5
sborrego <sborrego@users.noreply.github.com>
2018-01-21 17:57:05 -0800

    Update 03_basic_dir.sh

--- 03_basic_dir.sh
+++ 03_basic_dir.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-mkdir hello_directory
-echo "Hello, world!" > ./hello_directory/hello_file.txt
+
+mkdir hello_directory # makes new directory (file)
+echo "Hello, world!" > ./hello_directory/hello_file.txt # prints to file in indicated path
