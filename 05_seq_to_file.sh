ef4cd134e9d8df4ac8bab1390e16219437037b7f
sborrego <sborrego@users.noreply.github.com>
2018-01-22 03:05:40 -0800

    Update 05_seq_to_file.sh

--- 05_seq_to_file.sh
+++ 05_seq_to_file.sh
@@ -2,5 +2,5 @@
 
 for NUMBER in `seq 1 1 10`; do # seq is surrounded by back quotes
         echo ${NUMBER} # prints number sequence to stdout
-        echo ${NUMBER} >> seq.txt # prints number sequence to file. Using >> apends each number in the sequence to the file. If you use > it will overwrite the file with each number and the final file will only have one number (10, in this example).
+        echo ${NUMBER} >> seq1.txt # prints number sequence to file. Using >> apends each number in the sequence to the file. If you use > it will overwrite the file with each number and the final file will only have one number (10, in this example).
 done
