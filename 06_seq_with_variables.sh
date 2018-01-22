f781e71870511b8da425f89cdf733177b0734f39
Bioinformatics Support Group <35555041+bioinformaticssg@users.noreply.github.com>
2018-01-21 20:40:59 -0800

    Update 06_seq_with_variables.sh

--- 06_seq_with_variables.sh
+++ 06_seq_with_variables.sh
@@ -6,7 +6,7 @@ NSTART_SEQ=0
 NSTEP_SEQ=5
 NEND_SEQ=30
 
-for NUMBER in `seq ${NSTART_SEQ} ${NSTEP_SEQ} ${NEND_SEQ}`; do
+for NUMBER in `seq ${NSTART_SEQ} ${NSTEP_SEQ} ${NEND_SEQ}`; do # seq is surrounded by back quotes
         echo $NUMBER # prints number sequence to stdout (terminal)
         echo $NUMBER >> seq.txt # prints number sequence to file
 done
