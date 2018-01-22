1452024d7161fbd2ac64b579a0fe7520a0be5217
sborrego <sborrego@users.noreply.github.com>
2018-01-22 02:32:41 -0800

    Update 06_seq_with_variables.sh

--- 06_seq_with_variables.sh
+++ 06_seq_with_variables.sh
@@ -8,5 +8,5 @@ NEND_SEQ=30
 
 for NUMBER in `seq ${NSTART_SEQ} ${NSTEP_SEQ} ${NEND_SEQ}`; do # seq is surrounded by back quotes
         echo ${NUMBER} # prints number sequence to stdout (terminal)
-        echo ${NUMBER} >> seq.txt # prints number sequence to file
+        echo ${NUMBER} >> seq2.txt # prints number sequence to file
 done
