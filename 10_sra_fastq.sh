0d12c56342a8c360ce9476ca3ad97e15383d431c
Bioinformatics Support Group <35555041+bioinformaticssg@users.noreply.github.com>
2018-01-22 20:58:06 -0800

    added path

--- 10_sra_fastq.sh
+++ 10_sra_fastq.sh
@@ -1,16 +1,18 @@
 
 #!/bin/bash
 
-#$ -N sra_data_dump_10  # name of the job
-#$ -o sra.out           # contains what would normally be printed to stdout (the terminal)
-#$ -e sra.err           # file name to print standard error messages to. These messages would normally be printed to the the terminal
-#$ -q free64,som,asom   # request cores from the free64, som, asom queues.
-#$ -pe openmp 8-64      # request parallel environment. You can include a minimum and maximum core count.
-#$ -m beas              # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
-#$ -ckpt blcr           # (c)checkpoint: writes a snapshot of a process to disk, (r)estarts the process after the checkpoint is complete
+#$ -N sra_data_dump_10      # name of the job
+#$ -o ./sra_files/sra.out   # contains what would normally be printed to stdout (the terminal)
+#$ -e ./sra_files/sra.err   # file name to print standard error messages to. These messages would normally be printed to the the terminal
+#$ -q free64,som,asom       # request cores from the free64, som, asom queues.
+#$ -pe openmp 8-64          # request parallel environment. You can include a minimum and maximum core count.
+#$ -m beas                  # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
+#$ -ckpt blcr               # (c)checkpoint: writes a snapshot of a process to disk, (r)estarts the process after the checkpoint is complete
 
 module load blcr
 module load SRAToolKit
+
+mkdir ./sra_files
 
 # Retrieving SRA data and converting to fastq format
 # -X prints only the first 5 spots (20 lines)
