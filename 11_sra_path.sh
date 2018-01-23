18d8013e11636de5b36d52fd546474b3eecbee11
sborrego <sborrego@users.noreply.github.com>
2018-01-22 23:01:02 -0800

    Update 11_sra_path.sh

--- 11_sra_path.sh
+++ 11_sra_path.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 
 #$ -N sra_data_dump_11      # name of the job
-#$ -o ${SRA_DIR}/sra.out    # contains what would normally be printed to stdout (the terminal)
-#$ -e ${SRA_DIR}/sra.err    # file name to print standard error messages to. These messages would normally be printed to the the terminal
+#$ -o /data/users/${USER}/BioinformaticsSG/Working-on-the-HPC/sra_files/sra.out    # contains what would normally be printed to stdout (the terminal)
+#$ -e /data/users/${USER}/BioinformaticsSG/Working-on-the-HPC/sra_files/sra.err    # file name to print standard error messages to. These messages would normally be printed to the the terminal
 #$ -q free64,som,asom       # request cores from the free64, som, asom queues.
 #$ -pe openmp 8-64          # request parallel environment. You can include a minimum and maximum core count.
 #$ -m beas                  # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
@@ -12,12 +12,11 @@
 module load blcr
 module load SRAToolKit
 
+DIR=/data/users/${USER}/BioinformaticsSG/Working-on-the-HPC/sra_files
+DATA=${DIR}/data
 
-DIR=/data/users/${USER}/BioinformaticsSG/Working-on-the-HPC
-SRA_DIR=${DIR}/sra_files
-
-cd ${DIR}
-mkdir ${SRA_DIR}
+mkdir ${DATA}
+cd ${DATA}
 
 # Retrieving SRA data and converting to fastq format
 # -X prints only the first 5 spots (20 lines)
