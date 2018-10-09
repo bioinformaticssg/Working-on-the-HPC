93b0deb4d5f5738523c676e78a44afdcbe8f9bc0
Stacey Borrego <sborrego@users.noreply.github.com>
2018-10-09 14:52:19 -0700

    Update 11_sra_path.sh

--- 11_sra_path.sh
+++ 11_sra_path.sh
@@ -13,15 +13,15 @@ module load blcr
 module load SRAToolKit
 
 # Here we are assigning variables with paths
-DIR=/data/users/$USER/BioinformaticsSG/Working-on-the-HPC/sra_files
-DATA=${DIR}/data
+DIR=/data/users/$USER/BioinformaticsSG/Working-on-the-HPC
+DATA=${DIR}/sra_files/data
 
 # Here we are making a folder using the path we assigned above
-mkdir ${DATA}
+mkdir -p ${DATA}
 # Here we are changing our current directory to the new file
-cd ${DATA}
+# cd ${DATA}
 
 # Retrieving SRA data and converting to fastq format
 # -X prints only the first 5 spots (20 lines)
 # Data will show up in the new directory as SRR390728.fastq
-fastq-dump -X 5 SRR390728
+fastq-dump -X 5 ${DATA}/SRR390728
