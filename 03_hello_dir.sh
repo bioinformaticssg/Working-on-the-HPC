6e22c9c691670e1477d6cb944b69ee617be69a45
Stacey Borrego <sborrego@users.noreply.github.com>
2018-10-09 18:40:21 -0700

    Update 03_hello_dir.sh

--- 03_hello_dir.sh
+++ 03_hello_dir.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir hello_directory # makes new directory (file)
+mkdir hello_directory # makes new directory (folder)
 echo "Hello, world!" > ./hello_directory/hello_file.txt # prints to file in indicated path
